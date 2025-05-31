bce91a954c6bc75f204467158d79157d2a022d48
Anzhari Purnomo <10259593+anzharip@users.noreply.github.com>
2025-05-31 09:24:28 +0900

    fix: update venv path

--- chopper_plot.sh
+++ chopper_plot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 script_path=$(realpath $(echo $0))
 repo_path=$(dirname ${script_path})
-source ${repo_path}/.venv/bin/activate
+source /opt/venv/bin/activate
 exec python3 ${repo_path}/chopper_plot.py "$@"
